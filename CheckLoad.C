cc3077d3552f90667c02cfae073e639a137bf734
Yannick Patois <yannick.patois@iphc.cnrs.fr>
2024-11-20 16:33:25 +0100

    Works

--- CheckLoad.C
+++ CheckLoad.C
@@ -1,14 +1,14 @@
 R__ADD_INCLUDE_PATH(local/include)
 R__ADD_LIBRARY_PATH(local/lib64)
-R__LOAD_LIBRARY(libDictExample.so) 
+R__LOAD_LIBRARY(libDictExample.so)
 void CheckLoad() {
-//gSystem->Load("libDictExample");
-Simple s;
-cout << s.GetX() << endl;
-TFile *_file = TFile::Open("tmp.root", "RECREATE");
-gDirectory->WriteObject(&s, "MyS");
-Simple *MyS = nullptr;
-gDirectory->GetObject("MyS", MyS);
-cout << MyS->GetX() << endl;
-_file->Close();
+  // gSystem->Load("libDictExample");
+  Simple s;
+  cout << s.GetX() << endl;
+  TFile *_file = TFile::Open("tmp.root", "RECREATE");
+  gDirectory->WriteObject(&s, "MyS");
+  Simple *MyS = nullptr;
+  gDirectory->GetObject("MyS", MyS);
+  cout << MyS->GetX() << endl;
+  _file->Close();
 }
