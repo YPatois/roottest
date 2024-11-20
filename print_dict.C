e477a04b11cdf1be0c8b0ec2634103a4a570c6a5
Yannick Patois <yannick.patois@iphc.cnrs.fr>
2024-11-20 16:33:35 +0100

    stuff

--- print_dict.C
+++ print_dict.C
@@ -1,5 +1,20 @@
 void print_dict() {
-    TFile *_file = TFile::Open("local/lib64/libDictExample_rdict.pcm","READ");
-    _file->ls();
-}
+  TFile *_file = TFile::Open("local/lib64/libDictExample_rdict.pcm", "READ");
+  _file->ls();
 
+  std::cout << "___________________________________" << std::endl;
+  TIter it(_file->GetListOfKeys());
+  TKey *o;
+  while ((o = (TKey *)it())) {
+    std::cout << " ---------- " << std::endl;
+    o->Dump();
+    TProtoClass *o2 = (TProtoClass *)o->ReadObj();
+    o2->Dump();
+    //std::cout << o2->fName << std::endl;
+    /*TIter it2(o->GetListOfKeys());
+     TObject *o2;
+     while ((o2 = (TKey *)it2())) {
+       o2->Print();
+     }*/
+  }
+}
